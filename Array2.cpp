0db3a55aec6b6bac47c13a009326e0c6fac7a52e
Vyasaditi123 <90205422+Vyasaditi123@users.noreply.github.com>
2021-09-12 15:22:37 +0530

    Create Array2.cpp

---
+++ Array2.cpp
@@ -0,0 +1,14 @@
+//create an array of subjects you are studying in current semester
+
+#include<iostream.h>
+#include<conio.h>
+Using namespace std;
+int main()
+{
+ Char *subjects[7]={"Data communication",Discrete structure","Data structure and algorithm","Unix and shell programming","cloud computing","Application development using java programming","web development lab"};
+ for (int i=0;i<7;i++)
+ {
+   Cout<<subjects[i]<<"\n";
+ }
+   return 0;
+}
